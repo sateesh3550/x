0bee061b670befd6bffb68e7dd80b0f36769d707
root <root@sateesh35502.mylabserver.com>
2017-04-16 23:02:57 +0000

    test kitchene example

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -13,6 +13,10 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+#describe port(80), :skip do
+#  it { should_not be_listening }
+#end
+
+describe package('cowsay') do
+ it { should be_installed }
 end
